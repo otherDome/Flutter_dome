d10167dbb99317daa864baf9069210bf292e7793
北斋 <1038886097@qq.com>
2023-06-14 19:17:34 +0800

    feat: 1.一个flutter实例

--- lib/pages/Toolbar/home/index.dart
+++ lib/pages/Toolbar/home/index.dart
@@ -9,20 +9,82 @@ class Home extends StatefulWidget {
   State<StatefulWidget> createState() => _Home();
 }
 
-class _Home extends State<Home> {
+class _Home extends State<Home> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    )..repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('玩法探索'),
+        title: const Text('sol 星球效果'),
         centerTitle: true,
-        backgroundColor: const Color.fromRGBO(26, 188, 156, 1),
-        elevation: 5, //阴影
-        shadowColor: const Color(0xFF444654), //阴影颜色
+        backgroundColor: Colors.black,
       ),
-      body: const Center(
-        child: Text('这里是个星球'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  height: 300,
+                  child: CustomPaint(
+                    painter: CanvasWeb(_animationController),
+                  ),
+                ),
+                Positioned(
+                  top: 300,
+                  width: double.infinity,
+                  height: 300,
+                  child: Container(
+                    color: Colors.black,
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: const Text(
+                      '我的个人中心逻辑',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
+}
+
+class CanvasWeb extends CustomPainter {
+  final Animation<double> animation;
+
+  CanvasWeb(this.animation) : super(repaint: animation);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawRect(
+      Offset.zero & size,
+      Paint()..color = Colors.black,
+    );
+    // 执行绘制逻辑
+    // 在这里实现类似CAVS的动画效果
+  }
+
+  @override
+  bool shouldRepaint(CanvasWeb oldDelegate) => false;
 }
