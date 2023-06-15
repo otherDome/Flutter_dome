20b26c88739b24eea93bc944b7ce6a2625527d10
北斋 <1038886097@qq.com>
2023-06-15 20:00:27 +0800

    cli: 1.更新pnpm

--- lib/pages/Toolbar/home/index.dart
+++ lib/pages/Toolbar/home/index.dart
@@ -1,67 +1,133 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 import 'package:logger/logger.dart';
+import 'dart:math' as math;
+import 'package:vector_math/vector_math.dart' as vector;
 
-final logger = Logger();
+final logger = Logger(); // 日志
+Random random = Random(); // 随机数
+const double interval = 1.5; //组成圆圈的间隔
 
 class Home extends StatefulWidget {
+  //入口文件
   const Home({super.key});
   @override
   State<StatefulWidget> createState() => _Home();
 }
 
 class _Home extends State<Home> with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
+  bool isMale = true; // 默认为男性
+  int numberOfPeople = random.nextInt(300) + 1; // 1-1000随机数
+  late AnimationController _controller; // 动画控制器
+  late List<Point3D> points; //3D点集合
+  double rotationAngle = 0.0; // 旋转角度
+  final int rotationDurationInSeconds = 10; //动画时间
 
+  //动画开始
   @override
   void initState() {
-    super.initState();
-    _animationController = AnimationController(
-      duration: const Duration(seconds: 5),
+    super.initState(); //初始化
+    _controller = AnimationController(
+      //动画控制器
+      duration: Duration(seconds: rotationDurationInSeconds),
       vsync: this,
-    )..repeat(reverse: true);
+    )..repeat();
+
+    _controller.repeat();
+
+    //_controller.forward(); // 启动动画
+    points = generateSpherePoints(numberOfPeople, radius: 100); // 生成3D点集合
+    // 监听动画的变化
+    _controller.addListener(() {
+      setState(() {
+        rotationAngle = _controller.value * 2 * math.pi; // 更新旋转角度
+      });
+    });
   }
 
+  //动画销毁
   @override
   void dispose() {
-    _animationController.dispose();
-    super.dispose();
+    //销毁
+    _controller.dispose(); //销毁动画控制器
+    super.dispose(); //销毁
   }
 
+  List<Point3D> generateSpherePoints(int numPoints, {double radius = 1.0}) {
+    // 生成3D点集合
+    final points = <Point3D>[];
+
+    for (int i = 0; i < numPoints; i++) {
+      final phi = math.acos(-1 + (2 * i.toDouble()) / numPoints);
+      final theta = math.pi * (1 + math.sqrt(5)) * i.toDouble();
+      final x = radius * math.cos(theta) * math.sin(phi);
+      final y = radius * math.sin(theta) * math.sin(phi);
+      final z = radius * math.cos(phi);
+      final point3D = Point3D(x, y, z);
+      points.add(point3D);
+    }
+
+    return points;
+  }
+
+  //页面布局
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('sol 星球效果'),
+        title: const Text('玩法探索'),
         centerTitle: true,
-        backgroundColor: Colors.black,
+        backgroundColor: const Color.fromRGBO(26, 188, 156, 1),
       ),
       body: Column(
         children: [
-          Expanded(
-            child: Stack(
-              alignment: Alignment.topCenter,
-              children: [
-                SizedBox(
-                  width: double.infinity,
-                  height: 300,
-                  child: CustomPaint(
-                    painter: CanvasWeb(_animationController),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 0.0, horizontal: 15.0),
+                  child: Text(
+                    '在线人数:${numberOfPeople.toString()}人',
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(fontSize: 16.0),
                   ),
                 ),
-                Positioned(
-                  top: 300,
-                  width: double.infinity,
-                  height: 300,
-                  child: Container(
-                    color: Colors.black,
-                    padding: const EdgeInsets.symmetric(vertical: 16),
-                    child: const Text(
-                      '我的个人中心逻辑',
-                      style: TextStyle(fontSize: 18, color: Colors.white),
-                    ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 5.0, horizontal: 15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(isMale ? '男生' : '女生'),
+                      Switch(
+                        value: isMale,
+                        inactiveThumbColor: Colors.pinkAccent,
+                        inactiveTrackColor:
+                            isMale ? Colors.blue[200] : Colors.pinkAccent[200],
+                        activeColor: Colors.blue,
+                        onChanged: (bool newValue) {
+                          setState(() {
+                            isMale = newValue;
+                          });
+                        },
+                      ),
+                    ],
                   ),
                 ),
-              ],
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 350,
+            width: double.infinity,
+            child: Container(
+              color: Colors.black, // 背景色
+              child: CustomPaint(
+                painter: SpherePainter(rotationAngle, points),
+              ),
             ),
           ),
         ],
@@ -70,21 +136,44 @@ class _Home extends State<Home> with SingleTickerProviderStateMixin {
   }
 }
 
-class CanvasWeb extends CustomPainter {
-  final Animation<double> animation;
+class Point3D {
+  double x, y, z;
+  Point3D(this.x, this.y, this.z);
+}
 
-  CanvasWeb(this.animation) : super(repaint: animation);
+class SpherePainter extends CustomPainter {
+  // 画布
+  final double rotationAngle;
+  final List<Point3D> points;
 
+  SpherePainter(this.rotationAngle, this.points);
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.drawRect(
-      Offset.zero & size,
-      Paint()..color = Colors.black,
-    );
-    // 执行绘制逻辑
-    // 在这里实现类似CAVS的动画效果
+    final center = Offset(size.width / 2, size.height / 2);
+    final isRadius = size.width / 2.7;
+    final paint = Paint()
+      ..color = Colors.black
+      ..style = PaintingStyle.fill;
+    canvas.drawCircle(center, isRadius, paint);
+    const projectionDistance = 1000;
+    for (final point3D in points) {
+      final rotationMatrix = vector.Matrix4.rotationY(rotationAngle);
+      final rotatedPoint = vector.Vector3(point3D.x, point3D.y, point3D.z)
+        ..applyMatrix4(rotationMatrix);
+
+      final x = rotatedPoint.x;
+      final y = rotatedPoint.y;
+      final z = rotatedPoint.z;
+      final scale = interval * projectionDistance / (projectionDistance - z);
+      final projectedX = center.dx + x * scale;
+      final projectedY = center.dy + y * scale;
+      final point = Offset(projectedX, projectedY);
+      canvas.drawCircle(point, 3, Paint()..color = Colors.red);
+    }
   }
 
   @override
-  bool shouldRepaint(CanvasWeb oldDelegate) => false;
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
 }
--- lib/pages/Toolbar/index.dart
+++ lib/pages/Toolbar/index.dart
@@ -4,49 +4,80 @@ import 'package:flutter_dome/pages/Toolbar/equipment/index.dart';
 import 'package:flutter_dome/pages/Toolbar/my/index.dart';
 
 class Toolbar extends StatefulWidget {
-  const Toolbar({super.key});
+  const Toolbar({Key? key}) : super(key: key);
 
   @override
-  State<Toolbar> createState() => _ToolbarState();
+  State<StatefulWidget> createState() => _ToolbarState();
 }
 
 class _ToolbarState extends State<Toolbar> {
-  int cuttentIndex = 0;
-  final PageController _pageController = PageController();
+  int currentIndex = 0;
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(initialPage: currentIndex);
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        controller: _pageController,
+      body: PageView.builder(
+        controller: pageController,
         physics: const NeverScrollableScrollPhysics(),
-        children: const [Home(), Equipment(), My()],
+        itemBuilder: (context, index) {
+          return IndexedStack(
+            index: currentIndex,
+            children: const [
+              Home(),
+              Equipment(),
+              My(),
+            ],
+          );
+        },
+        onPageChanged: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
       ),
       bottomNavigationBar: BottomNavigationBar(
-          selectedItemColor: const Color.fromRGBO(26, 188, 156, 1),
-          unselectedItemColor: const Color.fromRGBO(204, 204, 204, .9),
-          currentIndex: cuttentIndex,
-          type: BottomNavigationBarType.fixed,
-          onTap: (index) {
-            setState(() {
-              cuttentIndex = index;
-              _pageController.jumpToPage(index);
-            });
-          },
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: '搜索',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.devices),
-              label: '设备',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: '我的',
-            ),
-          ]),
+        selectedItemColor: const Color.fromRGBO(26, 188, 156, 1),
+        unselectedItemColor: const Color.fromRGBO(204, 204, 204, .9),
+        currentIndex: currentIndex,
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+            pageController.animateToPage(
+              index,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+            );
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: '搜索',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.devices),
+            label: '设备',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '我的',
+          ),
+        ],
+      ),
     );
   }
 }
